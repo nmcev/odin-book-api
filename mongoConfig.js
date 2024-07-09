42ce3d9e63755d5b5ce56663232b1822575d1737
nmcev <1student.muhammed1@gmail.com>
2024-07-09 17:34:29 +0300

    feat: setup mongoDB connection

---
+++ mongoConfig.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const MongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
+
+// dotenv
+require('dotenv').config();
+
+async function connectToMongoDB() {
+
+  try {
+    await mongoose.connect(MongoUri)
+    console.log(`MongoDB successfully connected to ${MongoUri}`)
+
+  } catch (e) {
+    console.error('Error connecting to MongoDB:', e);
+
+  }
+}
+
+
+module.exports = connectToMongoDB;
