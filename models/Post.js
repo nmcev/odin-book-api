b8869ae356b59f8e84246deb34dca1a3537026d1
nmcev <1student.muhammed1@gmail.com>
2024-07-10 23:07:18 +0300

    feat: create schema post

---
+++ models/Post.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    author: { type: Schema.Types.ObjectId, ref: 'User', require: true },
+    content: { type: String, require: true },
+    media: { type: String, require: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    likes: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+})
+
+module.exports = mongoose.model('Post', postSchema);
