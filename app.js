80f0c9338b670916df0b9de012ed23b37e566b8a
nmcev <1student.muhammed1@gmail.com>
2024-07-10 21:38:11 +0300

    feat: setup local strategy for authentication

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const connectToMongoDB = require('./mongoConfig')
-
+const session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const passport = require('passport')
+
+
 
 var app = express();
 
@@ -14,25 +18,41 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// dotenv
+require('dotenv').config();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: true,
+  resave: false
+
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use the auth function
+require('./auth/index')(passport);
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', authRouter);
 
 //connect to mongodb
 connectToMongoDB().catch(err => console.error(err));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ auth/index.js
@@ -0,0 +1,43 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User');
+
+module.exports = function (passport) {
+
+    passport.use(new LocalStrategy({ usernameField: "username" }, async (username, password, done) => {
+
+        try {
+
+            const user = await User.findOne({ username });
+
+            if (!user) return done(null, false, { message: "Incorrect username." });
+
+
+            const isMatch = await bcryptjs.compare(password, user.password);
+
+            if (!isMatch) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+
+            return done(null, user);
+
+        } catch (error) {
+
+            console.log(error)
+        }
+    }))
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    })
+
+    passport.deserializeUser(async (userId, done) => {
+        try {
+            const user = await User.findById(userId)
+            done(null, user)
+        } catch (err) {
+            console.log(err)
+        }
+    })
+
+}
