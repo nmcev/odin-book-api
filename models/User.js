49f3bf036e99f18e95a72b70116161d314b17e31
nmcev <1student.muhammed1@gmail.com>
2024-07-10 22:54:50 +0300

    feat: ref to 'follow request' schema

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
     followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
     following: [{ type: Schema.Types.ObjectId, ref: "User" }],
     posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
-    followRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    followRequests: [{ type: Schema.Types.ObjectId, ref: "FollowRequest" }],
     repostedPosts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
 });
 
