f1a83628f30ef50ce00f5baaf46cc97daffc6463
nmcev <1student.muhammed1@gmail.com>
2024-07-11 19:11:27 +0300

    feat: setup all comments controllers

---
+++ controllers/commentsControllers.js
@@ -0,0 +1,37 @@
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
+
+exports.createComment_post = async (req, res, next) => {
+    const { content, postId, userId } = req.body;
+
+    try {
+        const newComment = new Comment({
+            content,
+            post: postId,
+            author: userId
+        })
+
+        await newComment.save();
+
+
+        await Post.findByIdAndUpdate(postId, { $push: { comments: newComment } });
+        res.json({ commented: true })
+    } catch (err) {
+        next(err)
+    }
+}
+
+exports.deleteComment_del = async (req, res, next) => {
+
+    const { commentId, postId } = req.params;
+
+    try {
+        await Comment.findByIdAndDelete(commentId);
+        await Post.findByIdAndUpdate(postId, { $pull: { comments: commentId } });
+
+        res.json({ deleted: true })
+    } catch (e) {
+        next(e)
+    }
+}
+
