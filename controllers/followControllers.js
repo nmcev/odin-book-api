783119437900c729b445350ba7f093aa1680c622
nmcev <1student.muhammed1@gmail.com>
2024-07-12 22:49:21 +0300

    feat: implement controllers of follows' routes

---
+++ controllers/followControllers.js
@@ -0,0 +1,43 @@
+const User = require('../models/User')
+
+exports.followUser_post = async (req, res, next) => {
+    const { userId } = req.params; // ID of user who will be followed
+    const currentUserId = req.user.id;
+
+
+    if (currentUserId === userId) {
+        return res.status(400).json({ message: 'You cannot follow yourself' });
+    }
+
+    try {
+        await User.findByIdAndUpdate(userId, { $addToSet: { followers: currentUserId } },);
+
+        await User.findByIdAndUpdate(currentUserId, { $addToSet: { following: userId } },);
+
+        res.status(200).json({ message: 'Followed user successfully' });
+
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+
+exports.unfollowUser_del = async (req, res, next) => {
+    const { userId } = req.params; // ID of user who will be unfollowed
+    const currentUserId = req.user.id;
+
+
+    try {
+        await User.findByIdAndUpdate(userId, { $pull: { followers: currentUserId } });
+
+        await User.findByIdAndUpdate(currentUserId, { $pull: { following: userId } });
+
+
+        res.status(200).json({ message: 'Unfollowed user successfully' });
+    } catch (error) {
+        next(error);
+    }
+};
+
