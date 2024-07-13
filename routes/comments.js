53b09160d1ff290be7bf78bb140901f88f85fda8
nmcev <1student.muhammed1@gmail.com>
2024-07-13 19:41:15 +0300

    feat(comments' routes): use is authenticated middleware before comments controllers

--- routes/comments.js
+++ routes/comments.js
@@ -4,12 +4,13 @@ const {
     createComment_post,
     deleteComment_del
 } = require('../controllers/commentsControllers');
+const { isAuthenticated } = require('../auth/isAuth');
 
 // CREATE a comment
-router.post('/comments', createComment_post) // todo: user should be authenticated to comment!(just use the isAuthenticated middleware);
+router.post('/comments', isAuthenticated, createComment_post)
 
 // DELETE a comment
-router.delete('/comments/:postId/:commentId', deleteComment_del);
+router.delete('/comments/:postId/:commentId', isAuthenticated, deleteComment_del);
 
 
 module.exports = router;
