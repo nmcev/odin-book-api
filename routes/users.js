60ccdf843d49a98c926d174d6a55d601c989897e
nmcev <1student.muhammed1@gmail.com>
2024-08-10 23:54:07 +0300

    feat(user router): create search endpoint for searching

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers_get, getAUser_get } = require('../controllers/usersControllers');
+const { getAllUsers_get, getAUser_get, search_get } = require('../controllers/usersControllers');
 
 // GET all USERS 
 router.get('/users', getAllUsers_get);
@@ -8,5 +8,8 @@ router.get('/users', getAllUsers_get);
 
 // GET a user
 router.get('/users/:username', getAUser_get);
+
+// GET search for users
+router.get('/search', search_get);
 
 module.exports = router;
