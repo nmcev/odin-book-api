d3672f2715021ba311f85989208fec19c163d82a
nmcev <1student.muhammed1@gmail.com>
2024-08-11 20:14:05 +0300

    feat(search controller):  implement search controller

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -15,16 +15,16 @@ exports.getAllUsers_get = async (req, res, next) => {
 exports.getAUser_get = async (req, res, next) => {
     try {
 
-        
+
         const user = await User.findOne({ username: req.params.username }).populate('followers').populate('following')
-        .populate({
-            path: 'posts',
-            populate: { path: 'author', select: 'username name profilePic' }
-        })  
-        .populate({
-            path: 'repostedPosts',
-            populate: { path: 'author', select: 'username name profilePic' }
-        })
+            .populate({
+                path: 'posts',
+                populate: { path: 'author', select: 'username name profilePic' }
+            })
+            .populate({
+                path: 'repostedPosts',
+                populate: { path: 'author', select: 'username name profilePic' }
+            })
             .select('-password');
         if (!user) return res.status(404).send('User not found');
 
@@ -32,5 +32,23 @@ exports.getAUser_get = async (req, res, next) => {
 
     } catch (error) {
         next(error)
+    }
+}
+
+exports.search_get = async (req, res, next) => {
+    const { username } = req.query;
+
+
+    try {
+        const users = await User.find({ username: { $regex: username, $options: 'i' } });
+
+        if (users.length === 0) {
+            return res.status(404).json({ message: 'No users found' });
+        }
+        res.json(users);
+
+    } catch (e) {
+
+        next(e);
     }
 }
