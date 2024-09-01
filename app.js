0c483779dd72f7aace3a93107de963ad03222bcd
nmcev <1student.muhammed1@gmail.com>
2024-09-01 15:27:39 +0300

    refactor: use a session secret

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
   store: sessionStore,
-  secret: 'cats',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
